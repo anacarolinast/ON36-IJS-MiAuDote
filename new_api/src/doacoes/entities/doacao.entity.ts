85789d32c721c06635edfa5236e30e2f9f330623
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:17:58 -0300

    add: atualização da entidade doacao

--- new_api/src/doacoes/entities/doacao.entity.ts
+++ new_api/src/doacoes/entities/doacao.entity.ts
@@ -1,1 +1,22 @@
-export class Doacao {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('doacoes')
+export class Doacao {
+    @PrimaryGeneratedColumn()
+    id: number; 
+
+    @Column()
+    doador_id: number; 
+
+    @Column()
+    data_doacao: Date; 
+
+    @Column({ length: 255 })
+    tipo_doacao: string;  
+
+    @Column('decimal', { precision: 10, scale: 2 })
+    valor_estimado: number;  
+
+    @Column()
+    gasto_id: number;  
+}
