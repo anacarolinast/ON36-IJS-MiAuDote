cd748b7b86e414266d1cdd717515573b271c8e8f
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:24:18 -0300

    add: atualização da entidade medicamento

--- new_api/src/medicamentos/entities/medicamento.entity.ts
+++ new_api/src/medicamentos/entities/medicamento.entity.ts
@@ -1,1 +1,22 @@
-export class Medicamento {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('medicamentos')
+export class Medicamento {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    animal_id: number;
+
+    @Column()
+    data_compra: Date;
+
+    @Column({ length: 255 })
+    descricao: string;
+
+    @Column()
+    veterinario_id: number;
+
+    @Column()
+    gasto_id: number;
+}
