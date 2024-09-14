f84fe24150b99e4cdeeb1b35cd9d3e6bae5b48f1
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:15:43 -0300

    add: atualização da entidade consumivel

--- new_api/src/consumiveis/entities/consumivel.entity.ts
+++ new_api/src/consumiveis/entities/consumivel.entity.ts
@@ -1,1 +1,16 @@
-export class Consumivel {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('consumiveis')
+export class Consumivel {
+    @PrimaryGeneratedColumn()
+    id: number; 
+
+    @Column({ length: 255 })
+    tipo_animal: string;  
+
+    @Column({ length: 255 })
+    descricao: string; 
+
+    @Column()
+    gasto_id: number; 
+}
