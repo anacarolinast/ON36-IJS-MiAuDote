8c8818e905487c8a88489ea54f15de36fd6f2f4a
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 00:42:53 -0300

    add: atualizando dto da entidade doacao e validação com class-validator

--- new_api/src/doacoes/dto/update-doacao.dto.ts
+++ new_api/src/doacoes/dto/update-doacao.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDoacaoDto } from './create-doacao.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateDoacaoDto extends PartialType(CreateDoacaoDto) {}
+export class UpdateDoacaoDto extends PartialType(CreateDoacaoDto) {
+    @IsOptional()
+    id?: number;
+}
