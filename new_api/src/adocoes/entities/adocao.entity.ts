0dec1741984ed9f52d172b0ae7f32bf2c5abd0c2
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:01:51 -0300

    add: atualização da entidade adocoes

--- new_api/src/adocoes/entities/adocao.entity.ts
+++ new_api/src/adocoes/entities/adocao.entity.ts
@@ -1,1 +1,23 @@
-export class Adocao {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("adocoes")
+export class Adocao {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    adotante_id: number;
+
+    @Column()
+    animal_id: number;
+
+    @Column()
+    data_adocao: Date;
+
+    @Column()
+    condicoes_especiais: string;
+
+    @Column()
+    status_aprovacao: number;
+
+}
