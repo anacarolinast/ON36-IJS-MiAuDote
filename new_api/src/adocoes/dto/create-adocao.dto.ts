c828706917b90808020ebe005ff32188f06cadc1
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 00:21:34 -0300

    add: atualizando dto da entidade adocao e validação com class-validator

--- new_api/src/adocoes/dto/create-adocao.dto.ts
+++ new_api/src/adocoes/dto/create-adocao.dto.ts
@@ -1,1 +1,22 @@
-export class CreateAdocaoDto {}
+import { IsNumber, IsDate, IsString, Length, Min } from 'class-validator';
+
+export class CreateAdocaoDto {
+    @IsNumber()
+    @Min(1) 
+    adotante_id: number;
+
+    @IsNumber()
+    @Min(1)  
+    animal_id: number;
+
+    @IsDate()  
+    data_adocao: Date;
+
+    @IsString()
+    @Length(0, 255) 
+    condicoes_especiais: string;
+
+    @IsString()
+    @Length(1, 255)
+    status_aprovacao: string;
+}
--- new_api/src/adocoes/dto/update-adocao.dto.ts
+++ new_api/src/adocoes/dto/update-adocao.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAdocaoDto } from './create-adocao.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateAdocaoDto extends PartialType(CreateAdocaoDto) {}
+export class UpdateAdocaoDto extends PartialType(CreateAdocaoDto) {
+    @IsOptional()
+    id?: number;
+}
