9440210c73909cf0cb4f0c15f16ad77142d374e7
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:28:16 -0300

    add: atualização da entidade vacina

--- new_api/src/vacinas/entities/vacina.entity.ts
+++ new_api/src/vacinas/entities/vacina.entity.ts
@@ -1,1 +1,22 @@
-export class Vacina {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('vacinas')
+export class Vacina {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    animal_id: number;
+
+    @Column()
+    data_vacinacao: Date;
+
+    @Column({ length: 255 })
+    tipo_vacina: string;
+
+    @Column()
+    veterinario_id: number;
+
+    @Column()
+    gasto_id: number;
+}
