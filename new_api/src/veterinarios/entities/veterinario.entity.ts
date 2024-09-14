49e16311967e47e9e8bffada2e67479bd9b631ad
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:29:56 -0300

    add: atualização da entidade vacina

--- new_api/src/veterinarios/entities/veterinario.entity.ts
+++ new_api/src/veterinarios/entities/veterinario.entity.ts
@@ -1,1 +1,16 @@
-export class Veterinario {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('veterinarios')
+export class Veterinario {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 255 })
+    especialidade: string;
+
+    @Column({ length: 10 })
+    registro_crmv: string;
+
+    @Column()
+    pessoa_id: number;
+}
