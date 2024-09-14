ac282a23982dbf5fc26f39bd86101f4757f40cbb
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:06:28 -0300

    add: atualização da entidade adotante

--- new_api/src/adotantes/entities/adotante.entity.ts
+++ new_api/src/adotantes/entities/adotante.entity.ts
@@ -1,1 +1,16 @@
-export class Adotante {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("adotante")
+export class Adotante {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    renda: number;
+
+    @Column()
+    condicao_entrevista: string;
+
+    @Column()
+    pessoa_id: number;
+}
