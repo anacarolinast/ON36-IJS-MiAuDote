8d9a32f1006f1f117d88be40e1d1ae6a59d19024
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 00:38:01 -0300

    add: atualizando dto da entidade castracao e validação com class-validator

--- new_api/src/castracoes/dto/update-castracao.dto.ts
+++ new_api/src/castracoes/dto/update-castracao.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCastracaoDto } from './create-castracao.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateCastracaoDto extends PartialType(CreateCastracaoDto) {}
+export class UpdateCastracaoDto extends PartialType(CreateCastracaoDto) {
+    @IsOptional()
+    id?: number;
+}
