61601a12b806c19b72f24e143ed6b77acc272d13
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:20:47 -0300

    add: atualização da entidade doador

--- new_api/src/doadores/entities/doador.entity.ts
+++ new_api/src/doadores/entities/doador.entity.ts
@@ -1,1 +1,13 @@
-export class Doadore {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('doadores')
+export class Doador {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 255 })
+    tipo_adocao: string;
+
+    @Column()
+    pessoa_id: number;
+}
