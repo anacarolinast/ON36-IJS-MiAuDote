457874e041b4737466a0ddf26d6ef76e9f46cc70
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:26:34 -0300

    add: atualização da entidade pessoa

--- new_api/src/pessoas/entities/pessoa.entity.ts
+++ new_api/src/pessoas/entities/pessoa.entity.ts
@@ -1,1 +1,22 @@
-export class Pessoa {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('pessoas')
+export class Pessoa {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 255 })
+    nome: string;
+
+    @Column({ length: 255 })
+    endereco: string;
+
+    @Column('text') 
+    telefone: string[];
+
+    @Column({ length: 255 })
+    email: string;
+
+    @Column({ length: 255 })
+    cpf: string;
+}
