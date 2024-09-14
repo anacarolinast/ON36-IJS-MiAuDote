64c55134a3cb4d1b5aa1a2dd5fff31fddb7b7a65
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:22:42 -0300

    add: atualização da entidade gasto

--- new_api/src/gastos/entities/gasto.entity.ts
+++ new_api/src/gastos/entities/gasto.entity.ts
@@ -1,1 +1,19 @@
-export class Gasto {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('gastos')
+export class Gasto {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    data_gasto: Date;
+
+    @Column({ length: 255 })
+    tipo: string;
+
+    @Column('int')
+    quantidade: number;
+
+    @Column('decimal', { precision: 10, scale: 2 }) 
+    valor: number;
+}
