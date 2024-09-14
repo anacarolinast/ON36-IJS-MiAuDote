095445a07a2ef2d9584e2de28415629b6e831097
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-13 23:51:19 -0300

    add: atualizando dto da entidade pessoas e validação com class-validator

--- new_api/src/pessoas/dto/update-pessoa.dto.ts
+++ new_api/src/pessoas/dto/update-pessoa.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePessoaDto } from './create-pessoa.dto';
+import { IsOptional} from 'class-validator'
 
-export class UpdatePessoaDto extends PartialType(CreatePessoaDto) {}
+export class UpdatePessoaDto extends PartialType(CreatePessoaDto) {
+    @IsOptional()
+    id?: number;
+}
