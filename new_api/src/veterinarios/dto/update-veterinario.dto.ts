9a795998cd9373a6ae0de38bdc4afbd2407aea2c
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 00:08:11 -0300

    add: atualizando dto da entidade veterinario e validação com class-validator

--- new_api/src/veterinarios/dto/update-veterinario.dto.ts
+++ new_api/src/veterinarios/dto/update-veterinario.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateVeterinarioDto } from './create-veterinario.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateVeterinarioDto extends PartialType(CreateVeterinarioDto) {}
+export class UpdateVeterinarioDto extends PartialType(CreateVeterinarioDto) {
+    @IsOptional()
+    id?: number;
+}
