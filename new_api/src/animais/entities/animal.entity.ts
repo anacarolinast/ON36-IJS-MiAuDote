452e3c6c91a8fb5f86e5f61c0855128b87014661
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:11:27 -0300

    add: atualização da entidade animal

--- new_api/src/animais/entities/animal.entity.ts
+++ new_api/src/animais/entities/animal.entity.ts
@@ -1,1 +1,25 @@
-export class Animal {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('animais')
+export class Animal {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 255 })
+    nome: string;
+
+    @Column({ length: 255 })
+    especie: string; 
+
+    @Column({ length: 1 })
+    sexo: string; 
+
+    @Column({ type: 'date' })
+    data_nascimento: Date; 
+
+    @Column({ length: 255 })
+    condicao_saude: string; 
+
+    @Column({ length: 255 })
+    estado_adocao: string;
+}
