57a429a2aae51378ec184746b3f20fe9fd3446b4
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 01:14:08 -0300

    add: atualização da entidade castracao

--- new_api/src/castracoes/entities/castracao.entity.ts
+++ new_api/src/castracoes/entities/castracao.entity.ts
@@ -1,1 +1,22 @@
-export class Castracao {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('castracoes')
+export class Castracao {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    animal_id: number; 
+
+    @Column({ type: 'date' })
+    data_castracao: Date; 
+
+    @Column({ length: 255 })
+    condicao_pos: string;  
+
+    @Column()
+    veterinario_id: number; 
+
+    @Column()
+    gasto_id: number; 
+}
