7cd0594cad73d3c6c774daf48757a2a5f847137c
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-14 00:49:51 -0300

    add: atualizando dto da entidade gasto e validação com class-validator

--- new_api/src/gastos/dto/create-gasto.dto.ts
+++ new_api/src/gastos/dto/create-gasto.dto.ts
@@ -1,1 +1,18 @@
-export class CreateGastoDto {}
+import { IsDate, IsNumber, IsString, Length, Min } from 'class-validator';
+
+export class CreateGastoDto {
+    @IsDate()  
+    data_gasto: Date;
+
+    @IsString()
+    @Length(1, 255) 
+    tipo: string;
+
+    @IsNumber()
+    @Min(0) 
+    quantidade: number;
+
+    @IsNumber()
+    @Min(0)  
+    valor: number;
+}
--- new_api/src/gastos/dto/update-gasto.dto.ts
+++ new_api/src/gastos/dto/update-gasto.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateGastoDto } from './create-gasto.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateGastoDto extends PartialType(CreateGastoDto) {}
+export class UpdateGastoDto extends PartialType(CreateGastoDto) {
+    @IsOptional()
+    id?: number;
+}
