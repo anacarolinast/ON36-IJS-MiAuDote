4f696910fc1cd220d3046d58f04149f952623a8a
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-15 19:27:35 -0300

    feat: ajustando modulo + service + controller de adocoes

--- new_api/src/adocoes/adocoes.controller.ts
+++ new_api/src/adocoes/adocoes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, Put, Delete } from '@nestjs/common';
 import { AdocoesService } from './adocoes.service';
 import { CreateAdocaoDto } from './dto/create-adocao.dto';
 import { UpdateAdocaoDto } from './dto/update-adocao.dto';
@@ -8,27 +8,27 @@ export class AdocoesController {
   constructor(private readonly adocoesService: AdocoesService) {}
 
   @Post()
-  create(@Body() createAdocaoDto: CreateAdocaoDto) {
+  async create(@Body() createAdocaoDto: CreateAdocaoDto) {
     return this.adocoesService.create(createAdocaoDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.adocoesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.adocoesService.findOne(+id);
+  async findOne(@Param('id') id: number) {
+    return this.adocoesService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: number, @Body() updateAdocaoDto: UpdateAdocaoDto) {
-    return this.adocoesService.update(+id, updateAdocaoDto);
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() updateAdocaoDto: UpdateAdocaoDto) {
+    return this.adocoesService.update(id, updateAdocaoDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.adocoesService.remove(+id);
+  async remove(@Param('id') id: number) {
+    return this.adocoesService.remove(id);
   }
 }
--- new_api/src/adocoes/adocoes.service.ts
+++ new_api/src/adocoes/adocoes.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Adocao } from './entities/adocao.entity';
 import { CreateAdocaoDto } from './dto/create-adocao.dto';
 import { UpdateAdocaoDto } from './dto/update-adocao.dto';
 
 @Injectable()
 export class AdocoesService {
-  create(createAdocaoDto: CreateAdocaoDto) {
-    return 'This action adds a new adocao';
+  constructor(
+    @InjectRepository(Adocao)
+    private readonly adocaoRepository: Repository<Adocao>,
+  ) {}
+
+  async create(createAdocaoDto: CreateAdocaoDto): Promise<Adocao> {
+    const adocao = this.adocaoRepository.create(createAdocaoDto);
+    return this.adocaoRepository.save(adocao);
   }
 
-  findAll() {
-    return `This action returns all adocoes`;
+  async findAll(): Promise<Adocao[]> {
+    return this.adocaoRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} adocao`;
+  async findOne(id: number): Promise<Adocao> {
+    return this.adocaoRepository.findOne({ where: { id } });
   }
 
-  update(id: number, updateAdocaoDto: UpdateAdocaoDto) {
-    return `This action updates a #${id} adocao`;
+  async update(id: number, updateAdocaoDto: UpdateAdocaoDto): Promise<Adocao> {
+    await this.adocaoRepository.update(id, updateAdocaoDto);
+    return this.adocaoRepository.findOne({ where: { id } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} adocao`;
+  async remove(id: number): Promise<{ id: number }> {
+    await this.adocaoRepository.delete(id);
+    return { id };
   }
 }
