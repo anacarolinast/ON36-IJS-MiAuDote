c7f2f24e3d8c2c883711cebc7d0dca429d590397
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-19 23:06:55 -0300

    add: ajuste pos refactor para o domain entidade pessoas

--- new_api/src/pessoas/domain/pessoas.ts
+++ new_api/src/pessoas/domain/pessoas.ts
@@ -1,3 +1,7 @@
+import { Doador } from 'src/doadores/domain/doadores';
+import { Veterinario } from 'src/veterinarios/domain/veterinarios';
+import { Adotante } from 'src/adotantes/domain/adotante';
+
 export class Pessoa {
     constructor(
         public readonly id: number,
@@ -6,8 +10,8 @@ export class Pessoa {
         public readonly telefone: string[],
         public readonly email: string,
         public readonly cpf: string,
-        public readonly veterinario?: string,
-        public readonly adotante?: string,
-        public readonly doador?: string,
+        public readonly veterinario?: Veterinario,
+        public readonly adotante?: Adotante,
+        public readonly doador?: Doador,
     ) {}
 }
