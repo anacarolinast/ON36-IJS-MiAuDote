d946b109eb5c910e1b42679f833cb582a1c47890
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-19 21:14:44 -0300

    add: implement in-file repository methods for module Vacinas

--- new_api/src/vacinas/infrastructure/persistence/in-file/repositories/vacina.repository.ts
+++ new_api/src/vacinas/infrastructure/persistence/in-file/repositories/vacina.repository.ts
@@ -5,25 +5,59 @@ import { VacinaEntity } from "../entities/vacina.entity";
 
 @Injectable()
 export class InFileVacinaRepository implements VacinaRepository {
+    private readonly vacinas = new Map<number, VacinaEntity>();
+    private idCounter = 1;
+
     async save(vacina: Vacina): Promise<Vacina> {
-        throw new Error('Method not implemented.');
+        const vacinaEntity = new VacinaEntity();
+        vacinaEntity.id = this.idCounter++;
+        vacinaEntity.animal_id = vacina.animal_id;
+        vacinaEntity.data_vacinacao = vacina.data_vacinacao;
+        vacinaEntity.tipo_vacina = vacina.tipo_vacina;
+        vacinaEntity.veterinario_id = vacina.veterinario_id;
+        vacinaEntity.gasto_id = vacina.gasto_id;
+
+        this.vacinas.set(vacinaEntity.id, vacinaEntity);
+
+        console.log(`Vacina criada com sucesso!`); 
+        return vacinaEntity;
     }
 
     async findAll(): Promise<Vacina[]> {
-        throw new Error('Method not implemented.');
+        console.log("Listando todas as vacinas...");
+        return Array.from(this.vacinas.values());
     }
 
     async findById(id: number): Promise<Vacina | null> {
-        throw new Error('Method not implemented.');
+        const vacina = this.vacinas.get(id);
+        if (vacina) {
+            console.log(`Vacina encontrada: ${vacina.id}`);
+            return vacina;
+        } else {
+            console.log(`Vacina com ID ${id} não encontrado.`);
+            return null;
+        }
     }
 
     async update(id: number, vacina: Partial<Vacina>): Promise<Vacina | null> {
-        throw new Error('Method not implemented.');
+        const existingVacina = this.vacinas.get(id);
+        if (existingVacina) {
+            const updatedVacina = { ...existingVacina, ...vacina };
+            this.vacinas.set(id, updatedVacina);
+            console.log(`Vacina com ID ${id} atualizada com sucesso!`);
+            return updatedVacina;
+        } else {
+            console.log(`Vacina com ID ${id} não encontrada para atualização.`);
+            return null;
+        }
     }
 
     async remove(id: number): Promise<void> {
-        throw new Error('Method not implemented.');
+        if (this.vacinas.has(id)) {
+            this.vacinas.delete(id);
+            console.log(`Vacina com ID ${id} removida com sucesso!`);
+        } else {
+            console.log(`Vacina com ID ${id} não encontrada para remoção.`);
+        }
     }
-
-    private readonly vacina = new Map<String, VacinaEntity>();
 }
