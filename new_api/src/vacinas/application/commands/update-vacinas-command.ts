db25c770001525068bc650bcf82ab7e6d024356a
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-19 21:29:23 -0300

    add: implement in-file repository methods for module Veterinarios

---
+++ new_api/src/vacinas/application/commands/update-vacinas-command.ts
@@ -0,0 +1,10 @@
+export class CreateVacinaCommand {
+    constructor(
+      public readonly id: number,
+      public readonly adotante_id?: number,
+      public readonly animal_id?: number,
+      public readonly data_adocao?: Date,
+      public readonly condicoes_especiais?: string,
+      public readonly status_aprovacao?: string
+    ) {}
+  }
--- new_api/src/veterinarios/infrastructure/persistence/in-file/repositories/veterinario.repository.ts
+++ new_api/src/veterinarios/infrastructure/persistence/in-file/repositories/veterinario.repository.ts
@@ -1,29 +1,61 @@
 import { Injectable } from "@nestjs/common";
 import { VeterinarioRepository } from "src/veterinarios/application/ports/veterinarios.repository";
 import { Veterinario } from "src/veterinarios/domain/veterinarios";
-
+import { VeterinarioEntity } from "../entities/veterinario.entity";
 
 @Injectable()
 export class InFileVeterinarioRepository implements VeterinarioRepository {
+    private readonly veterinarios = new Map<number, VeterinarioEntity>();
+    private idCounter = 1;
+
     async save(veterinario: Veterinario): Promise<Veterinario> {
-        throw new Error('Method not implemented.');
+        const veterinarioEntity = new VeterinarioEntity();
+        veterinarioEntity.id = this.idCounter++;
+        veterinarioEntity.especialidade = veterinario.especialidade;
+        veterinarioEntity.registro_crmv = veterinario.registro_crmv;
+        veterinarioEntity.pessoa_id = veterinario.pessoa_id;
+
+        this.veterinarios.set(veterinarioEntity.id, veterinarioEntity);
+
+        console.log(`Veterinario ${veterinarioEntity.id} criado com sucesso!`); 
+        return veterinarioEntity;
     }
 
     async findAll(): Promise<Veterinario[]> {
-        throw new Error('Method not implemented.');
+        console.log("Listando todos os veterinarios...");
+        return Array.from(this.veterinarios.values());
     }
 
     async findById(id: number): Promise<Veterinario | null> {
-        throw new Error('Method not implemented.');
+        const veterinario = this.veterinarios.get(id);
+        if (veterinario) {
+            console.log(`Veterinario encontrado: ${veterinario.id}`);
+            return veterinario;
+        } else {
+            console.log(`Veterinario com ID ${id} não encontrada.`);
+            return null;
+        }
     }
 
     async update(id: number, veterinario: Partial<Veterinario>): Promise<Veterinario | null> {
-        throw new Error('Method not implemented.');
+        const existingVeterinario = this.veterinarios.get(id);
+        if (existingVeterinario) {
+            const updatedVeterinario = { ...existingVeterinario, ...veterinario };
+            this.veterinarios.set(id, updatedVeterinario);
+            console.log(`Veterinario com ID ${id} atualizado com sucesso!`);
+            return updatedVeterinario;
+        } else {
+            console.log(`Veterinario com ID ${id} não encontrado para atualização.`);
+            return null;
+        }
     }
 
     async remove(id: number): Promise<void> {
-        throw new Error('Method not implemented.');
+        if (this.veterinarios.has(id)) {
+            this.veterinarios.delete(id);
+            console.log(`Veterinario com ID ${id} removido com sucesso!`);
+        } else {
+            console.log(`Veterinario com ID ${id} não encontrado para remoção.`);
+        }
     }
-
-    private readonly veterinario = new Map<String, Veterinario>();
 }
