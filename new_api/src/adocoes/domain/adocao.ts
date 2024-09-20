8d69fe5283384533299b1399d08d15b1139ea793
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-09-19 22:52:52 -0300

    add: ajuste pos refactor para o domain entidade adocao

--- new_api/src/adocoes/domain/adocao.ts
+++ new_api/src/adocoes/domain/adocao.ts
@@ -1,7 +1,5 @@
-// Ajustar conforme os refactories
-
-// import { Animal } from 'src/animal/entities/adocao.entity';
-// import { Adotante } from 'src/adotante/entities/adotante.entity';
+import { Adotante } from 'src/adotantes/domain/adotante';
+import { Animal } from 'src/animais/domain/animal';
 
 export class Adocao {
     constructor(
@@ -11,10 +9,8 @@ export class Adocao {
       public readonly data_adocao: Date,
       public readonly condicoes_especiais: string,
       public readonly status_aprovacao: string,
-
-    // Ajustar conforme os refactories
-    //   public readonly animal?: Animal[],
-    //   public readonly adotante?: Adotante,
+      public readonly animal?: Animal[],
+      public readonly adotante?: Adotante,
 
     ) {}
   }
