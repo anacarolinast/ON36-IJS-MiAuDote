1e7677b66997e8558e3d16c1bf53d9c7079c5b71
mlsmourao <167831762+mlsmourao@users.noreply.github.com>
2024-10-16 19:59:23 -0300

    docs: gerando swagger para documentação da api

--- new_api/src/main.ts
+++ new_api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(
@@ -8,6 +9,7 @@ async function bootstrap() {
       driver: 'typeorm'
     }),
   );
+  
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: false,
@@ -15,6 +17,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("MiAuDote")
+    .setDescription("API destinada para gerenciar custos de um abrigo de animais.")
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
